51234c540437f414f344dce1ae5803b539163348
Sairamdondapati <sairamdondapatii@gmail.com>
2025-01-06 08:03:32 +0530

    06/01/2025

---
+++ Notes_project/middleware/auth.middleware.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+
+const auth = (req,res,next)=>{
+    const token = req.headers.authorization.split(' ')[1];
+    // const token = req.query.token;
+    console.log(token)
+    if(token){
+        try {
+            jwt.verify(token, 'shhhhh', (error,decoded)=>{
+            if(decoded){
+                next()
+            }else{
+                res.status(400).send({'error':error.message})
+            }
+            })
+        } catch (error) {
+            console.log(error)
+            res.status(400).send({'error':error.message})
+        }
+
+    }else{
+        res.status(400).send({'message':'please login'})
+    }
+
+}
+
+module.exports = {
+    auth
+}
---
+++ Notes_project/model/Notes.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const NotesSchema = mongoose.Schema({
+    title:{type:String,required:true},
+    body:{type:String,required:true},
+    authour:{type:String,required:true},
+    category:{type:String,required:true}
+},{
+    versionKey: false
+});
+
+
+const NotesModel = mongoose.model('Notesdata',NotesSchema);
+
+module.exports = {
+    NotesModel,NotesSchema
+}
