ab8bec2d8d0eeba14149aa3c8a01fbb183f192bc
Sairamdondapati <sairamdondapatii@gmail.com>
2025-02-02 09:07:02 +0530

    02/02/2025

--- Day-9_email_authentication/index.js
+++ Day-9_email_authentication/index.js
@@ -4,6 +4,8 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const { userRouter } = require('./routes/UserRouter');
 const { auth } = require('./middleware/auth');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 const app = express();
 
 app.use(express.json());
@@ -16,6 +18,24 @@ app.use(
     })
   );
 
+const transporter = nodemailer.createTransport({
+    service:'Gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: process.env.USER,
+        pass: process.env.APP_PASS
+    }
+});
+
+
+function otpgenerator(){
+    let otp ='';
+    for(let i=0; i<6; i++){
+        otp = otp + Math.floor(Math.random()*10);
+    }
+    return otp;
+}
 
 app.get('/',(req,res)=>{
     res.send('WELCOME')
@@ -31,6 +51,43 @@ app.get("/protected", (req, res) => {
     } catch (error) {
       console.log(error);
       res.send(error);
+    }
+  });
+
+
+
+
+  app.get("/get-otp", async (req, res) => {
+    console.log(req.body);
+    const { email } = req.body;
+    const otp = otpgenerator();
+    try {
+      const result = await transporter.sendMail({
+        to: [email],
+        from: "dondapatiram34@gmail.com",
+        subject: "otp verification",
+        text: `your otp for the password reset process is ${otp}`,
+      })
+        console.log(result);
+        req.session.OTP = otp;
+        console.log("otp", req.session.OTP);
+        res.send("Email sent");
+    } catch (error) {
+        console.log("error", error);
+        console.log(error.message);
+        res.send("something wrong happend");
+    }
+  });
+  
+  app.get("/verify-otp", async (req, res) => {
+    const { OTP } = req.query;
+    const serverOtp = req.session.OTP;
+    console.log(OTP, serverOtp);
+  
+    if (OTP == serverOtp) {
+      res.send("OTP verified");
+    } else {
+      res.send("wrong otp");
     }
   });
 
