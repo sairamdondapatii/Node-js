daf9d3aef462bb95eebf6fc2f83fdf61dc168719
Sairamdondapati <sairamdondapatii@gmail.com>
2025-02-07 17:15:14 +0530

    07/02/2025

---
+++ Day-10_Socketio/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+
+app.use(express.json());
+
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer)
+
+app.get('/', (req,res)=>{
+    res.send('welcome port number 8090')
+})
+
+let count = 0;
+io.on("connection", (socket) => {
+    count++;
+    io.emit('newUser',count);
+    
+    socket.on('message',(sai)=>{
+        console.log('message',sai)
+        io.emit('usermsg',sai)
+    });
+
+
+    socket.on('disconnect',()=>{
+        count--;
+        io.emit('newUser',count);
+    })
+  });
+
+
+httpServer.listen(8090,()=>{
+    console.log('server running at http://localhost:8090');
+});
+
